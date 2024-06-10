0ff1808947e70f8e7f49fde831d47a354f1f0b70
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-10 17:40:55 +0530

    "Added User model and updated Message model with required fields and validation"

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -1,14 +1,46 @@
-import mongoose,{ Schema,Document } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-export interface Message extends Document{
-    content: String;
-    createdAt: Date
+export interface Message extends Document {
+  content: String;
+  createdAt: Date;
 }
 
-const MessageSchema:Schema<Message> = new Schema({
-    content: String,
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: String;
+  email: String;
+  password: String;
+  verifyCode: String;
+  verifyCodeExpiry: Date;
+  isAcceptingMessage: boolean;
+  message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verify,
 });
