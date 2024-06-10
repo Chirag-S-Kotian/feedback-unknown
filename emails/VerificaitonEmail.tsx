2a8ac2dd769b8cafecc77ccd05a77684b228ff18
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-10 22:49:08 +0530

    resend email component done

---
+++ emails/VerificaitonEmail.tsx
@@ -0,0 +1,12 @@
+import { Html,Head,Font,Preview,Heading,Row,Section,Text,Button } from "@react-email/components";
+
+interface VerificationEmailProps{
+    username:string;
+    otp:string;
+}
+
+export default function VerificationEmail({ username,otp }:VerificationEmailProps ){
+    return{
+        <Html lang="en" > 
+    }
+}
