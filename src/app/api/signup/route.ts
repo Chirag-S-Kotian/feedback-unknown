6d396acd3123a1c6e42d8eb83894b33d606dc232
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-13 15:50:39 +0530

    "Refactor user registration route to send verification email and handle response"

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -45,6 +45,7 @@ export async function POST(request: Request) {
       const hashedPassword = await bcryptjs.hash(password, 10);
       const expiryDate = new Date();
       expiryDate.setHours(expiryDate.getHours() + 1);
+
       const newUser = new UserModel({
         username,
         email,
@@ -55,23 +56,29 @@ export async function POST(request: Request) {
         isAcceptingMessage: true,
         messages: [],
       });
-      const savedUser = await newUser.save();
+      await newUser.save();
 
-      const updatedUser = await UserModel.findByIdAndUpdate(
-        savedUser._id,
-        {
-          verifyCode,
-          verifyCodeExpiry,
-        },
-        {
-          new: true,
-        }
+      //send verification email
+      const emailResponse = await sendVerificationEmail(
+        email,
+        username,
+        verifyCode
       );
-      await sendVerificationEmail(email, username, verifyCode);
+      if (!emailResponse.success) {
+        return Response.json(
+          {
+            success: false,
+            message: emailResponse.message,
+          },
+          {
+            status: 500,
+          }
+        );
+      }
       return Response.json(
         {
           success: true,
-          message: "User registered successfully",
+          message: "User registered successfully.Please verify your email",
         },
         {
           status: 201,
