fd6207d5dad214ffa3b1d67b33efcd75d5d8679f
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-13 14:53:21 +0530

    "Added ApiResponse interface property and updated sendVerificationEmail function signature"

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,3 +1,9 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificaitonEmail";
+import { ApiResponse } from "@/types/ApiResponse";
 
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+) {}
--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,8 +1,8 @@
-import { Message } from "@/model/User.model"; 
+import { Message } from "@/model/User.model";
 
-export interface ApiResponse{
-    status: boolean;
-    message: string;
-    isAcceptingMessages?: boolean;
-
+export interface ApiResponse {
+  status: boolean;
+  message: string;
+  isAcceptingMessages?: boolean;
+  messages?: Array<Message>;
 }
