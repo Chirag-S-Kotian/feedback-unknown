fa0aa31076d786edcf2b905c9c59e211dbdf0353
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-14 12:31:16 +0530

    "Updated signin page to use Next-Auth React hooks for session management and authentication"

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-const page = () => {
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email}
+        <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>page</div>
-  )
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
 }
-
-export default page
