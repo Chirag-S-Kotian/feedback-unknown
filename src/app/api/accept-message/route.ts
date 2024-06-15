b5915174fad094fcccf105debf6dcff07bab1672
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-15 22:27:03 +0530

    "Added GET endpoint to accept-message route to retrieve user data"

--- src/app/api/accept-message/route.ts
+++ src/app/api/accept-message/route.ts
@@ -66,3 +66,57 @@ export async function POST(request: Request) {
     );
   }
 }
+
+export async function GET(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "You are not logged in",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const userId = user._id;
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        user,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("failed to update user status to accept messages");
+    return Response.json(
+      {
+        success: false,
+        message: "failed to update user status to accept messages",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
