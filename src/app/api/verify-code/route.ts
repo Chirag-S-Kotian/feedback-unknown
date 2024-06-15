911bcf4b2ef2a7bd744eb6961ac981d52ef5b494
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-15 15:25:34 +0530

    route for verify-code just completed

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,74 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const data = await request.json();
+    const { username, code } = data;
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({
+      username: decodedUsername,
+    });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        {
+          success: true,
+          message: "User verified successfully",
+        },
+        {
+          status: 200,
+        }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification Code expired ,so please sign up again to get a new code",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid Verification Code",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("Error verifying user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
