92bd5b98d3b8c0b875a8ad03d69bb5533720488a
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-16 10:56:43 +0530

    "Refactor suggest-messages route: updated imports, removed maxDuration, changed OpenAI API usage, and modified error handling"

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,37 +1,32 @@
-import { openai } from "@ai-sdk/openai";
-import { StreamingTextResponse, streamText } from "ai";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   try {
-    const { messages } = await req.json();
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-    const result = await streamText({
-      model: openai("gpt-4-turbo"),
-      messages,
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      max_tokens: 400,
+      prompt,
     });
 
-    return result.toAIStreamResponse();
+    const completion = response.choices[0]?.text.trim();
+
+    return NextResponse.json({ completion });
   } catch (error) {
     if (error instanceof OpenAI.APIError) {
       const { name, status, headers, message } = error;
-      return NextResponse.json(
-        {
-          name,
-          status,
-          headers,
-          message,
-        },
-        {
-          status,
-        }
-      );
+      return NextResponse.json({ name, status, headers, message }, { status });
     } else {
-      console.error("An unexpected erroroccured", error);
+      console.error("An unexpected error occurred:", error);
       throw error;
     }
   }
