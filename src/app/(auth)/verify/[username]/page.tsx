b2e924571be99b56f5781603a983d07c9230abe4
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-16 22:24:26 +0530

    username page is ready

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,53 @@
+import { useToast } from "@/components/ui/use-toast";
+import { VerifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const VerifyAccount = () => {
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof VerifySchema>>({
+    resolver: zodResolver(VerifySchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof VerifySchema>) => {
+    try {
+      const response = await axios.post<ApiResponse>(`/api/verify-code`, {
+        username: params.username,
+        code: data.code,
+      });
+
+      toast({
+        title: "Success",
+        description: response.data.message,
+      });
+      router.replace("signin");
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Error",
+        description: errorMessage,
+        variant: "destructive",
+      });
+    }
+
+    return (
+      <div>
+        <h1>Verify Account</h1>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <input type="text" {...form.register("code")} />
+          <button type="submit">Verify</button>
+        </form>
+      </div>
+    );
+  };
+};
+
+export default VerifyAccount;
