768c1d8648f7d4e567e5d030c863d7984ac8703e
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-16 11:22:45 +0530

    "Added @radix-ui/react-toast and usehooks-ts dependencies, updated package-lock.json and package.json, and modified signin/page.tsx to use useState and useDebounceValue from usehooks-ts"

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 import {zodResolver} from "@hookform/resolvers/zod"
 import Link from "next/link";
+import { useState } from "react";
 import {useForm} from "react-hook-form";
 import * as z from "zod"
+ import {useDebounceValue} from 'usehooks-ts'
 
 
 const page = () => {
+  const [username,setUsername] = useState('')
+  const [usernameMessage,setUsernameMessage] = useState('')
+  const [isCheckingUsername,setIsCheckingUsername] = useState(false)
+  const [isSubmitting,setIsSubmitting] = useState(false)
+
+  const debouncedUsername = useDebounceValue(username,300)
+
   return (
     <div>
-      
+
     </div>
   )
 }
