4d13276784c472a3788e8b5da267956bd2126e9e
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-17 13:17:15 +0530

    "Update title metadata in various layout components"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "True Feedback",
+  title: "Phantom Feedback",
   description: "Real feedback from real people.",
 };
 
