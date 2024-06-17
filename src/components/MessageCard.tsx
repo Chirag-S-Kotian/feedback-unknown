96ff37f0198296bc39c016596b6714872ef1dc84
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-17 11:49:00 +0530

    "Added AlertDialog component to MessageCard, updated imports and card structure"

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,31 +1,54 @@
-"use client"
-import {Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle} from "@/components/ui/card"
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
 
 const MessageCard = () => {
   return (
     <Card>
-        <CardHeader>
-          <CardTitle>Message</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <CardDescription>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </CardDescription>
-        </CardContent>
-        <CardFooter>
-          <CardDescription>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </CardDescription>
-        </CardFooter>
-      </Card>
+      <CardHeader>
+        <CardTitle>Message</CardTitle>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="outline">Show Dialog</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        <CardDescription>Lorem</CardDescription>
+      </CardHeader>
+      <CardContent></CardContent>
+      <CardFooter></CardFooter>
     </Card>
-  )
-}
+  );
+};
 
-export default MessageCard
+export default MessageCard;
