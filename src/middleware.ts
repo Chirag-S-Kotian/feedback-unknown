6799497dc14a17306670150d7a968d6133b18f4e
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-17 14:39:11 +0530

    "Rearranged imports and config in middleware.ts, updated redirect logic in middleware function"

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-import { NextResponse, NextRequest } from "next/server";
 export { default } from "next-auth/middleware";
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/signin", "/signup", "/", "/verify/:path*"],
+};
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
+
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
   if (
     token &&
     (url.pathname.startsWith("/signin") ||
       url.pathname.startsWith("/signup") ||
       url.pathname.startsWith("/verify") ||
-      url.pathname.startsWith("/"))
+      url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+
   if (!token && url.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
+
   return NextResponse.next();
 }
-
-//see matching paths below to learn more
-export const config = {
-  matcher: ["/signin", "/signup", "/", "/dashboard/:path*", "/verify/:path*"],
-};
