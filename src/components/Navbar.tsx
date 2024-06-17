7ece31aefed3a905aae65189e62ad1809d8c8ec1
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-17 11:34:20 +0530

    "Updated Navbar component with new UI and authentication logic"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,36 @@
-"use client"
-import { useSession,signOut } from 'next-auth/react'
-import Link from 'next/link'
-import {User} from "next-auth"
-import React from 'react'
+"use client";
+import { useSession, signOut } from "next-auth/react";
+import Link from "next/link";
+import { User } from "next-auth";
+import React from "react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
-    const  {data: session} = useSession()
-    const user:User = session?.user
+  const { data: session } = useSession();
+  const user: User = session?.user;
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="p-4 md:p-6 shadow-md">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <a className="text-xl font-bold mb-4 md:mb-0" href="#">
+          Phantom Message
+        </a>
+        {session ? (
+          <>
+            <span className="mr-4">
+              Welcome , {user.username || user.email}
+            </span>
+            <Button className="w-full md:w-auto" onClick={() => signOut()}>
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Link href="/signin">
+            <Button className="w-full md:w-auto">Login</Button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
