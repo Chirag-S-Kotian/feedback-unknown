fad9c3c684104787eac59dd2462d7895f8dd8804
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-17 12:55:43 +0530

    "Removed comment from Mail import statement"

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Mail } from "lucide-react"; // Assuming you have an icon for messages
+import { Mail } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Autoplay from "embla-carousel-autoplay";
 import messages from "@/messages.json";
